406d4dc8f81bf3e40cc4420ad6a9ae26bd8684b4
LaibaKhalil61 <laibakhalil6194@gmail.com>
2024-11-10 05:04:58 +0500

    add scoring-answers logic

--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,5 @@
-from pydantic import BaseModel
-from enum import Enum
-from utils.MistralAIModel import client
-from prompts import generate_mock_interview_prompt,score_the_answers
 from fastapi import FastAPI
-import json
-
-# Data Model Classes 
-class difficulty(str,Enum):
-    easy = "easy"
-    medium = "medium"
-    hard = "hard"
-
-class InterviewFormSelection(BaseModel):
-    difficulty_level : difficulty
-    job_title : str
-    job_description : str
+from routes.route import router
 
 app = FastAPI()
-
-@app.post("/textualQuizzes/")
-async def create_new_textual_quiz(selection : InterviewFormSelection):
-    # generating questions
-    prompt = generate_mock_interview_prompt(selection.job_title,selection.job_description,selection.difficulty_level)
-    chat_response = client.chat.complete(
-        model = "mistral-large-latest",
-        messages = prompt
-    )
-    raw_response = chat_response.choices[0].message.content
-
-    # Cleaning the response 
-    cleaned_response = raw_response.strip('```json\n').strip('```')
-    cleaned_response = cleaned_response.replace(r'\n', '\n').replace(r'\"', '"')
-    json_data = json.loads(cleaned_response)
-
-    # making a new entry in the db
-    
-    return json_data
-    username = laibakhalil6194
-    password = 7y2ylmMobXIMNd0T
+app.include_router(router)
---
+++ backend/models/model.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List
+from enum import Enum 
+
+class Difficulty(str,Enum):
+    easy = "easy"
+    medium = "medium"
+    hard = "hard"
+
+class Score(BaseModel):
+    Tone: int
+    Clarity: int
+    Accuracy: int
+    Grammar: int
+    Feedback: str
+
+class InterviewFormSelection(BaseModel):
+    userID : str
+    difficulty_level : Difficulty
+    job_title : str
+    job_description : str
+
+class Answer(BaseModel):
+    answers : List[str]
+
