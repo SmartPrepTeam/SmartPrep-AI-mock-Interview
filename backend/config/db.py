98bb7799a9b04f20eea4670867d588d819bf73d2
LaibaKhalil61 <laibakhalil6194@gmail.com>
2024-11-17 14:53:10 +0500

    add lifespan handler in db.py

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,8 +1,20 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from utils.Constants import MONGODB_ATLAS
-client = MongoClient(MONGODB_ATLAS)
+from beanie import init_beanie
+from fastapi import FastAPI
+from models.textualQuiz import TextualQuiz
+from models.textualAnswer import TextualAnswer
 
-db = client.smartprep_db
+async def db_lifespan(app:FastAPI):
+    # on starting the app
+    app.mongodb_client = AsyncIOMotorClient(MONGODB_ATLAS)
+    app.database = app.mongodb_client["smartprep_db"]
+    await init_beanie(database = app.database, document_models = [TextualQuiz,TextualAnswer])
+    ping_response = await app.database.command("ping")
+    if int(ping_response["ok"]) != 1:
+        raise Exception("Problem connecting to database cluster.")
 
-textQuiz_collection = db["TextualQuiz"]
-textAns_collection = db["TextualAnswer"]
+    yield
+
+    # on shutting down the app
+    app.mongodb_client.close()
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routes.route import router
+from config.db import db_lifespan
 
-app = FastAPI()
+app = FastAPI(lifespan = db_lifespan)
+
 app.include_router(router)
